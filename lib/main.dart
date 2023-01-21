7e5dc12a5f64ed9af30903a58a4cb5d98f0756cc
miyaguiMode <richardrnmr@gmail.com>
2023-01-20 20:09:57 -0500

    numero veces presionado

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Texto cambiado abc',
+              'numero de veces presionado',
             ),
             Text(
               '$_counter',
